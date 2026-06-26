487d5212db0d3621b4dc239c1855725f6f4df5a9
EC2 Default User <ec2-user@ip-172-31-37-139.ap-south-1.compute.internal>
2026-06-26 08:19:09 +0000

    Fix PostgreSQL host for Docker deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
     user: "postgres",
-    host: "localhost",
+    host: "postgres",
     database: "foodapp",
     password: "postgres123",
     port: 5432
